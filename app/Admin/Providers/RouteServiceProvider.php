3154d94caefa198a19a29928d432bcab43f37cca
misu <misu@fullspeed.co.jp>
2017-02-05 01:49:24 +0900

    create admin table

--- app/Admin/Providers/RouteServiceProvider.php
+++ app/Admin/Providers/RouteServiceProvider.php
@@ -8,15 +8,6 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 class RouteServiceProvider extends ServiceProvider
 {
     /**
-     * This namespace is applied to your controller routes.
-     *
-     * In addition, it is set as the URL generator's root namespace.
-     *
-     * @var string
-     */
-    protected $namespace = 'App\Admin\Http\Controllers';
-
-    /**
      * Define your route model bindings, pattern filters, etc.
      */
     public function boot()
@@ -29,22 +20,6 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map()
     {
-        $this->mapApiRoutes();
-    }
-
-    /**
-     * Define the "api" routes for the application.
-     *
-     * These routes are typically stateless.
-     */
-    protected function mapApiRoutes()
-    {
-        Route::group([
-            'middleware' => 'api',
-            'namespace'  => $this->namespace,
-            'prefix'     => 'api/v1/admin',
-        ], function ($router) {
-            require base_path('app/Admin/routes/admin_api.php');
-        });
+        require base_path('app/Admin/routes/admin_api.php');
     }
 }
--- app/Admin/routes/admin_api.php
+++ app/Admin/routes/admin_api.php
@@ -4,11 +4,21 @@ use Illuminate\Http\Request;
 /**
  * api for admin panel
  */
-
-//Route::resource('user', 'UserController');
-
 $api = app('Dingo\Api\Routing\Router');
 
+/**
+ * authentication
+ */
+$api->version('v1', [], function ($api) {
+    $api->group(['prefix' => 'admin', 'namespace' => 'App\Admin\Http\Controllers\Auth'], function ($api) {
+        $api->get('/authenticate', 'AuthController@index');
+        $api->post('/authenticate', 'AuthController@auth');
+    });
+});
+
+
 $api->version('v1', ['namespace' => 'App\Admin\Http\Controllers\V1'], function ($api) {
-    $api->resource('users', 'UserController');
+    $api->group(['prefix' => 'admin'], function ($api) {
+        $api->resource('users', 'UserController');
+    });
 });
